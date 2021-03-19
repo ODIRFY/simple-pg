f8c1238e552196df459099853948fb7e9675ab79
ODIRF <odirf123@gmail.com>
2021-03-19 15:38:57 +0100

    V1.3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Simple password generator
-![Version: 1.2.0](https://img.shields.io/badge/Version-1.2.0-88d945?style=for-the-badge&logo=github)
+![Version: 1.3.0](https://img.shields.io/badge/Version-1.3.0-88d945?style=for-the-badge&logo=github)
 
 
 **simplepg is a *simple* password generator written in Python, with which you can generate passwords.**
@@ -7,12 +7,12 @@
 ---
 ## Usage
 
-- Clone this repository:
+- Clone the repository:
 ```git clone https://github.com/ODIRFY/simple-pg.git```
 
-- Navigate to the folder and then to `/src`
+- Navigate to the cloned repository
 
-- Run the generator:
-```python3 simplepg.py```
+- Run the generator (`main.py`):
+```python3 main.py```
 
 You will be asked how long the password should be, and the password will be generated.
--- src/simplepg.py
+++ src/simplepg.py
@@ -10,8 +10,8 @@ import os, time # import os and time
 
 # Password chars-options:
 chars = string.digits + string.ascii_letters + string.punctuation
-    
-class generator():
+
+def generator():
     print("Welcome to simplepg!")
     while True:
         # Waiting for input from user.
@@ -19,42 +19,37 @@ class generator():
         os.system('clear')
 
         try: 
-            userinput = int(input("Please enter the length of the password: \n"))
+            userinput = int(input("Please enter the length of your password: "))
+        
         # Prints an error if input is invalid.
         except:
-            print("Invalid Input. Please enter a number: ")
-            userinput = int(input(""))
+            os.system('clear')
+            userinput = int(input("Invalid Input. Please enter a number: "))
 
         # Prints the generated password and shows the lenth.
         os.system('clear')
-        time.sleep(1)
-        print("Generated password is: ")
-        print("————————————————————————————————————\n" + ''.join(secrets.choice(chars) for _ in range(userinput)) + "\n\n       (" + str(userinput) + " characters)\n" + "————————————————————————————————————\n\n"
+        time.sleep(0.4)
+        print("Generated password " + "(" + str(userinput) + " characters):\n")
+        print(''.join(secrets.choice(chars) for _ in range(userinput)) + "\n\n—————————————\n")
         
         # Prints a rating (in terms of length).
-        + str("Rating: "), end="")
-        
         if userinput == 0:
             print("A password with 0 characters does not exist.")
-            
-            
+                
         elif userinput >=24:
             print("The generated password is very very strong, it is impossible to crack it.")
-        
+            
         elif userinput >=8:
             print("The generated password is very secure, and almost impossible to crack.")
-
+        
         elif userinput <=8:
-            print("The generated password is very easy to crack, " +
-            "I recommend a length of at least 8 characters.")
+            print("The generated password is very easy to crack, I recommend a length of at least 8 characters.")    
         
         # ask for exit or resume
-        exit = input("\n——\nPress Enter to resume, or type 'q' for exit. ")
+        exit = input("\nPress Enter to resume, or type 'q' for exit: ")
 
         if exit == "q":
             os.system('clear')
-            time.sleep(0.2)
-            print("Exit!")
-            time.sleep(0.4)
+            time.sleep(0.1)
             os.system('clear')
             break
